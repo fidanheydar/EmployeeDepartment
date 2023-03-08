72eb8b4c9e67af100f16d6cccaf5e1724092ec69
fidannh <fidannh@code.edu.az>
2023-03-09 00:55:06 +0400

    task

---
+++ ConsoleApp12/Models/Department.cs
@@ -0,0 +1,67 @@
+﻿using ConsoleApp11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Models
+{
+    internal class Department
+    {
+        public string Name;
+        public int EmployeeCount = 0;
+        public Employee[] employees;
+        public readonly int EmployeeLimit = 2;
+        public readonly int SalaryLimit = 250;
+
+
+        public Department(string Name)
+        {
+            this.Name = Name;
+        }
+        public Employee[] AddEmployee()
+        {
+            Array.Resize(ref employees, EmployeeCount + 1);
+            Console.WriteLine("Enter the Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter the Surname");
+            string SurName = Console.ReadLine();
+            Console.WriteLine("Enter the Salary");
+            int Salary = int.Parse(Console.ReadLine());
+
+            if (Salary >= 250)
+            {
+                AddEmployee();
+            }
+            else
+            {
+                Console.WriteLine("Salary must be 250 or more than 250");
+            }
+            return employees;
+        }
+
+        public void ShowEmployee()
+        {
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine("Name:" + employee.Name, "Surname:" + employee.Surname,
+                    "Salary:" + employee.Salary);
+            }
+            bool NameCheck(string name, string surname)
+            {
+                if (!char.IsUpper(name[0]) || char.IsUpper(surname[0]))
+                {
+                    return false;
+                }
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(surname))
+                {
+                    return false;
+                }
+                return true;
+
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp12/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Models
+{
+    internal class Employee
+    {
+        public string Name;
+        public string Surname;
+        private int _salary;
+
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value >= 250)
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+        public Employee(string Name, string Surname, int Salary)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Salary = Salary;
+        }
+
+    }
+}
