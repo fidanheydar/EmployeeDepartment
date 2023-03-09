c5cb5152b47f4cec1cba00053512d49d2bf800f6
fidanheydar <125346195+fidanheydar@users.noreply.github.com>
2023-03-09 11:49:17 +0400

    Update Program.cs

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,46 +1,10 @@
-﻿using ConsoleApp11.Models;
+using ConsoleApp12.Models;
 using System.ComponentModel.Design;
 
-Employee employee = new Employee("Fidan", "Heydarova", 4000);
-Department department = new Department("");
+
+Department department= new Department();
 
 
-bool status = true;
 
-
-Console.WriteLine("Select");
-Console.WriteLine("1: Add Employee");
-Console.WriteLine("2: Show the Employees");
-Console.WriteLine("3: Exiting");
-
-string select = Console.ReadLine();
-
-
-if (select == "1")
-{
-    if (department.EmployeeCount > department.EmployeeLimit)
-    {
-        Console.WriteLine("Employee limit is 2 it is over");
-    }
-    else
-    {
-        department.AddEmployee();
-    }
-}
-else if (select == "2")
-{
-    department.ShowEmployee();
-}
-else if (select == "3")
-{
-    Console.WriteLine("Exit the program");
-}
-
-
-Console.WriteLine("Enter the Name");
-string Name = Console.ReadLine();
-Console.WriteLine("Enter the Surname");
-string SurName = Console.ReadLine();
-Console.WriteLine("Enter the Salary");
-int Salary = int.Parse(Console.ReadLine());
-
+department.AddEmployee();
+department.ShowEmployee();
