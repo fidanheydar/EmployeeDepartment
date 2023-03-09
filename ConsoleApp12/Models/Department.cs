1d0ac6f6c6f140c64fd82c7aa111f6bdf4d5cce1
fidanheydar <125346195+fidanheydar@users.noreply.github.com>
2023-03-09 11:51:06 +0400

    Update Department.cs

--- ConsoleApp12/Models/Department.cs
+++ ConsoleApp12/Models/Department.cs
@@ -1,67 +1,71 @@
-﻿using ConsoleApp11.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp11.Models
+namespace ConsoleApp12.Models
 {
-    internal class Department
+    public class Department
     {
-        public string Name;
-        public int EmployeeCount = 0;
-        public Employee[] employees;
-        public readonly int EmployeeLimit = 2;
-        public readonly int SalaryLimit = 250;
+        Employee[] employees = new Employee[0];
+        Employee emp;
+        public string name { get; set; }
+        public int SalaryLimit { get; set; } = 250;
+        public int EmployeeLimit { get; set; } = 2;
 
 
-        public Department(string Name)
+
+        public void AddEmployee()
         {
-            this.Name = Name;
-        }
-        public Employee[] AddEmployee()
-        {
-            Array.Resize(ref employees, EmployeeCount + 1);
-            Console.WriteLine("Enter the Name");
-            string Name = Console.ReadLine();
-            Console.WriteLine("Enter the Surname");
-            string SurName = Console.ReadLine();
-            Console.WriteLine("Enter the Salary");
-            int Salary = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= EmployeeLimit; i++)
+            {
+                Console.WriteLine("Enter the Name:");
+                string name=Console.ReadLine();
+                while (name.Any(char.IsDigit) || !char.IsUpper(name[0]) || name.Any(char.IsSymbol))
+                {
+                    Console.WriteLine("Name must contain letters");
+                    name = Console.ReadLine();
+                }
 
-            if (Salary >= 250)
-            {
-                AddEmployee();
+                Console.WriteLine("Enter the Surname:");
+                string surname = Console.ReadLine();
+                while  (surname.Any(char.IsDigit) ||!char.IsUpper(surname[0]) || surname.Any(char.IsSymbol))
+                    {
+                    Console.WriteLine("Surname must contain letters");
+                    surname= Console.ReadLine();
+                }
+                Console.WriteLine("Enter the salary:");
+                int salary=int.Parse(Console.ReadLine());
+                while(salary<SalaryLimit)
+                {
+                    Console.WriteLine("Salary must be 250 or more than 250");
+                    salary=int.Parse(Console.ReadLine());
+                }
+
+                if (employees.Length < EmployeeLimit)
+                {
+                    emp=new Employee(name,surname,salary);
+                    Array.Resize(ref employees, employees.Length+1);
+                    employees[employees.Length-1] = emp;
+                }
+                else
+                {
+                    Console.WriteLine("Limit is over");
+                }
+
+                
             }
-            else
-            {
-                Console.WriteLine("Salary must be 250 or more than 250");
-            }
-            return employees;
         }
 
         public void ShowEmployee()
         {
-            foreach (Employee employee in employees)
+            foreach (var employee in employees)
             {
-                Console.WriteLine("Name:" + employee.Name, "Surname:" + employee.Surname,
-                    "Salary:" + employee.Salary);
-            }
-            bool NameCheck(string name, string surname)
-            {
-                if (!char.IsUpper(name[0]) || char.IsUpper(surname[0]))
-                {
-                    return false;
-                }
-                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(surname))
-                {
-                    return false;
-                }
-                return true;
-
+                Console.WriteLine($"Name ; { employee.Name}  Surname:  {employee.Surname}");
             }
         }
-
     }
 }
+
